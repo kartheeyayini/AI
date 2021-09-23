75edd526ad7dcc8bf34ed5cec4cfda5e2854a6d7
unknown <kartheeyayini20@gmail.com>
2021-09-24 00:57:15 +0530

    first mess

---
+++ code_02.py
@@ -0,0 +1,23 @@
+apple=10
+banana=20
+print(apple+banana)
+print("apple" + "\nbanana")
+print("apple" + "\tbanana")
+
+x=15
+y=20
+print(x+y,x*y,x/y)
+print((x+y)*(x-y))
+print(x**3,y**5)
+print(x%y)
+print(x*y,x+(x/y))
+print(x*y,x+(x/y))
+print(x**2+y**2+2*(x*y))
+
+
+a="ravi"
+b="suga"
+print(a)
+print(b)
+
+
