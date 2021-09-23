0e4a102f48595189dc24ab02161fb291e16685c0
unknown <kartheeyayini20@gmail.com>
2021-09-23 19:55:43 +0530

    first commit

---
+++ code_01.py
@@ -0,0 +1,25 @@
+my_name="luna"
+my_name1="snow"
+my_name2="puppy"
+print("hello " + my_name + " " + my_name1 + " " + my_name2)
+
+# my name is karthika and my age is 21 and im in year 5
+
+myname="karthika"
+myage= "21"
+year= "5"
+print("my name is " + myname + " and my age is " + myage + " and im in year " + year)
+myfruits={"apple":80, "orange":60, "box":["ppa",320,10.5,"rin",[30,28.2,"bin",[42,{"mint":22,"garlic":15},"tomato",[30,20,10],"bett"]]]}
+print (myfruits["box"])
+print (myfruits["apple"])
+print("rate of apple is "+ str(myfruits["apple"]))
+mycars={"ford":"i10","i20":"vols","sorpio": 13.4,"punto":[24,"cars",35],"wagonar":{"car2":"car3","car4":6.7,"car7":[3,"rate",7.9,[56,7.98],{"a","b"},(59,"joke",80),{"grapes":50}]}
+add_n=int(myage)+int(year)
+print(add_n)
+print(type(add_n))
+print(type(myname))
+print(type(myage))
+
+
+
+
